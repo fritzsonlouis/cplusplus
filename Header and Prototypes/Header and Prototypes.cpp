a0fde260f2b689b54db85d634dd7a16353ddc4b5
fritzsonlouis <111246872+fritzsonlouis@users.noreply.github.com>
2023-01-17 20:19:54 -0500

    Add files via upload

---
+++ Header and Prototypes/Header and Prototypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void doSomething();
+
+int main()
+{
+	doSomething();
+	doSomething();
+
+
+	return 0;
+}
+
+void doSomething()
+{
+	cout << "Hello" << endl;
+}
